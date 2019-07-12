b4a1946b627f1d49a5354d90751197f550323918
Cauê Almeida <nncl@live.com>
2019-07-12 15:35:19 -0300

    Watching storage

---
+++ src/components/Watch.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Watch {{this.name}}</h1>
+</template>
+
+<script>
+    export default {
+        name: "Watch"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import persistentState from 'vue-persistent-state';
+
+const initialState = {
+    name: ''  // will get value from localStorage if found there
+};
+
+Vue.config.productionTip = false;
+Vue.use(persistentState, initialState);
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
